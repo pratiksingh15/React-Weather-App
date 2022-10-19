fd2e02773dab1bcbbec9b7272913a286c5c407f7
pratik <pratiksinghthakur34@gmail.com>
2022-10-19 15:48:58 +0530

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react'
+
+
+const Weather = () => {
+
+  const [search,setSearch] = useState('Bhopal');
+  const [data,setData] = useState([]);
+  const [input,setInput] = useState('');
+  let componentMounted = true;
+
+  useEffect(()=>{
+    const fetchWeather = async ()=>{
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e39061fedcac0a7ea596347ecc802964
+    `);
+    if(componentMounted){
+      setData(await response.json());      
+    }
+    return ()=>{
+      componentMounted = false;
+     }
+    }
+    fetchWeather();
+  },[search]);
+
+  let emoji = null;
+  if (typeof data.main !== "undefined"){
+    if(data.weather[0].main === "Clouds"){
+      emoji = "fas-cloud";
+    }else if(data.weather[0].main === "Thunderstorm"){
+      emoji = "fas-bolt";
+    }else if(data.weather[0].main === "Drizzle"){
+      emoji = "fas-cloud-rain";
+    }else if(data.weather[0].main === "Rain"){
+      emoji = "fas-cloud-shower-heavy";
+    }else if(data.weather[0].main === "Snow"){
+      emoji = "fas-snow-flake";
+    }else{
+      emoji = "fa-smog";
+    }
+  }else{
+    return(
+      <div>...Loading</div>
+    )
+  }
+
+  let temp = (data.main.temp - 273.15).toFixed(2);
+  let temp_max = (data.main.temp_max -273.15).toFixed(2);
+  let temp_min = (data.main.temp_min -273.15).toFixed(2);
+
+// Date
+  let d = new Date();
+  let date = d.getDate();
+  let year = d.getFullYear();
+  let month = d.toLocaleString("default",{month:'long'});
+  let day = d.toLocaleString("default",{weekday:'long'});
+
+// Time
+  let time = d.toLocaleString([],{
+    hour:'2-digit',
+    minute:'2-digit',
+    second:'2-digit'
+  })
+// Form handle
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input);    
+  }
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="card text-white text-center border-0">
+              <img src={`https://source.unsplash.com/random/600x800?${data.weather[0].main} `}className="card-img" alt="..." />
+              <div className="card-img-overlay">
+                <form onSubmit={handleSubmit}>
+                  <div className="input-group mb-4 w-75 mx-auto">
+                    <input type="search" className="form-control" placeholder="Search city" aria-label="Search City" aria-describedby="basic-addon2" name='search' value={input} onChange={(e)=> setInput(e.target.value)} required/>
+                    <button type='submit' className="input-group-text" id="basic-addon2">
+                      <i className="fas fa-search"></i>
+                    </button>
+                  </div>
+                </form>
+                <div className="bg-dark bg-opacity-50 py-2">
+                  <h2 className="card-title">
+                    {data.name}
+                  </h2>
+                  <p className="card-text lead">{day}, {month} {date},{year} <br /> {time}</p>
+                  <hr />
+                  <i className={`fas ${emoji} fa-4x mb-2`}></i>
+                  <h1 className='fw-bolder mb-4'>{temp} &deg;C</h1>
+                  <p className="lead fw-bolder mb-0">{data.weather[0].main}</p>
+                  <p className="lead">{temp_min}&deg;C | {temp_max}&deg;C  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Weather
